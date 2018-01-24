2b0176a0edeed32f31c0071835b0b6c29b1a085c
Mikko Loponen <mikko.loponen@outotec.com>
2018-01-24 15:52:31 +0200

    3.4 puhelinluettelon backend osa 4

--- index.js
+++ index.js
@@ -50,6 +50,12 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.delete('/api/persons/:id', (req, res) =>{
+    const id = Number(req.params.id)
+    persons = persons.filter(item => item.id !== id)
+    res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
