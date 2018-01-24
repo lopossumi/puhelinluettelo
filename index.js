6a679d5b0d4e31465ff02470861b68d2000432fb
Mikko Loponen <mikko.loponen@outotec.com>
2018-01-24 15:48:29 +0200

    3.3 puhelinluettelon backend osa 3

--- index.js
+++ index.js
@@ -31,6 +31,16 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello from puhelinluettelo!</h1>')
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(item => item.id === id)
+    if(person){
+        res.json(person)
+    }else{
+        res.status(404).end()
+    }
+})
+
 app.get('/info', (req,res) => {
     res.send(
         `Puhelinluettelossa ${persons.length} henkilön tiedot.<br/>${new Date()}`)
