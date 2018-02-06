3c0419f6e0d1533a5571d0a0c123e777e51e6c9e
Mikko Loponen <mikko.loponen@iki.fi>
2018-02-06 15:47:22 +0200

    Tehtävä 3.12: Tietokanta komentoriviltä

---
+++ mongo.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+// Read MongoDB URI from environment variable and create schema for Person.
+const url = process.env.MONGODB_URI
+const Person = mongoose.model('Person', {
+    name: String,
+    number: String
+ })
+
+const argCount = process.argv.length-2
+
+const storePerson = (name, number) => {
+    mongoose.connect(url)
+
+    const person = new Person({
+       name: name,
+       number: number
+    })
+    
+    person
+       .save()
+       .then(response => {
+         console.log(`Henkilö ${name} tallennettu tietokantaan numerolla ${number}.`)
+         mongoose.connection.close()
+    })
+}
+
+const listPersons = () => {
+    mongoose.connect(url)
+
+    Person.find({})
+    .then(result => {
+        console.log('Puhelinluettelo:')
+        result.forEach(person => {
+          console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+      })
+}
+
+// const formatPerson = (person) => {
+//     return {
+//         name:person.name,
+//         number:person.number
+//     }
+// }
+
+switch(true){
+    case argCount === 0:
+        listPersons()
+        break
+    case argCount === 2:
+        storePerson(process.argv[2], process.argv[3])
+        break
+    default:
+        console.log("wrong number of parameters.")
+        break
+}
